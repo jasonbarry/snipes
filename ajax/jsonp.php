ec988b0ad6ea820d2df0878b174918f21b709796
Jason Barry <jasongrantbarry@gmail.com>
2011-08-21 16:44:48 -0700

    Removed 'host' env key, redundant

--- ajax/jsonp.php
+++ ajax/jsonp.php
@@ -3,7 +3,6 @@
 header('Content-type: application/javascript');
 
 $data['ip'] = $_SERVER['REMOTE_ADDR'];
-$data['host'] = $_SERVER['REMOTE_HOST'];
 $data['ref'] = $_SERVER['HTTP_REFERER'];
 $data['lang'] = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
 
